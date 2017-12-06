5e4f119d90cc3ae9386eff885c26c4d82a496f21
sgzzy <witzzy@163.com>
2017-12-06 11:19:22 +0800

    add App file

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <vue-custom-table :thead="thead" :data="data"></vue-custom-table>
+  </div>
+</template>
+<script>
+import vueCustomTable from "./component/vue-table";
+import { thead, data } from "./mock/mock";
+export default {
+  name: "app",
+  components: { vueCustomTable },
+  data: function() {
+    return {
+      thead: thead,
+      data: data
+    };
+  }
+};
+</script>
+<style scoped>
+
+</style>
+
